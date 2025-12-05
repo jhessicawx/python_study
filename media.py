458e6caeacdb64c4ac1f02415161fbad17bfadeb
unknown <jlima81808@gmail.com>
2025-12-05 17:45:52 -0300

    Código de soma de 3 números reais(float) e a média entre eles

---
+++ media.py
@@ -0,0 +1,7 @@
+nota1 = float(input('Digite a 1º nota: '))
+nota2 = float(input('Digite a 2º nota: '))
+nota3 = float(input('Digite a 2º nota: '))
+
+media = (nota1 + nota2 + nota3)/3
+
+print('A média das notas será:', media)
